1044cedb094881a90c1fa253c5f4cec37afaf1af
Rubén López <ruben.lopez@radiatemedia.com>
2012-06-05 21:50:24 -0600

    A lot of refactoring.

--- lib/classes.rb
+++ lib/classes.rb
@@ -3,33 +3,14 @@ require 'active_record'
 require 'logger'
 require 'image_spooler.rb'
 
-class Offer
-  attr_accessor :id, :url, :complete
-  
-  DATA = 0.upto(100).map { |x| [x, "http://www.test_url.com/file_#{x}.jpg", rand(2)] }
+class Offer < ActiveRecord::Base
+  has_one :resized_offer
 
-  def initialize(id, url, completed)
-    @id, @url, @complete = id, url, completed
-  end
-
-  def self.find(id = 1)
-    Offer.new(DATA[id][0], DATA[id][1], DATA[id][2])  
-  end
-
-  def completed?
-    rand(2)
+  def self.fetch
+    find(:all, :limit => ResizedOffers::Config::bulk_count).select { |o| o.resized_offer == nil }
   end
 end
 
-class CroppedImage
-  attr_accessor :id, :s3_path, :file_name, :extension
-
-  def self.find(id = 1)
-    CroppedImage.new(1, "http://w3.amazon.com/dir1/90283/", "test_image", "jpg")
-  end
-
-  def initialize(id, s3_path, file_name, extension)
-    @id, @s3_path, @file_name, @extension = id, s3_path, file_name, extension
-  end
+class ResizedOffer < ActiveRecord::Base
+  belongs_to :offer
 end
-
--- spec/image_spooler_spec.rb
+++ spec/image_spooler_spec.rb
@@ -1,20 +1,43 @@
 require 'spec_helper'
-require 'image_spooler.rb'
+require "#{Dir.pwd}/lib/image_spooler.rb"
 
-describe ImageSpooler do
-	
+
+describe ResizedOffers::Config do
+
+  describe "Setting up configurations" do
+    it "Allows me to configure the spooler" do
+      ResizedOffers::Config.set do |conf|
+        conf.bulk_count = 10
+        conf.database   = 'lon_qa_test'
+        conf.sizes = { :small   => "100x200", 
+                       :medium  => "300x300",
+                       :large   => "500x500"
+                     }
+      end.should be_true
+    end
+
+    it "Allows me to pull out class instance variables" do
+      ResizedOffers::Config.database.should eq('lon_qa_test')
+      ResizedOffers::Config.bulk_count.should be_equal(10)
+      ResizedOffers::Config.sizes.class.should be_eql(Hash)
+    end
+  end
+
   describe "Establishing connection our data" do
-#   it "Establishes connection with the db" do
-#     ImageSpooler.establish_connection
-#     ActiveRecord::Base.connection.should_not raise_error(ActiveRecord::ConnectionNotEstablished)
-#   end
+   it "Establishes connection with the db" do
+     ActiveRecord::Base.connection.should_not raise_error(ActiveRecord::ConnectionNotEstablished)
+   end
 
    it "Maps our joined table to an object" do
-      eval("CroppedImage").should be_eql(CroppedImage)
+      eval("ResizedOffer").should be_eql(ResizedOffer)
     end
 
     it "Maps our 'offers' table to an object" do
       eval("Offer").should be_eql(Offer)
+    end
+
+    it "Checks association Offer.resized_offer" do
+      Offer.new.respond_to?(:resized_offer).should be_true
     end
   end
 
@@ -22,16 +45,44 @@ describe ImageSpooler do
     it "Returns an Offer object" do
       Offer.find(1).id.should be_eql(1)
     end
-
-    it "Returns a CroppedImage object" do
-      CroppedImage.find(1).id.should be_eql(1)
-    end
   end
 
 	describe "#fetch" do
-    it "Fetches only records that are marked as not completed." do
-      records = ImageSpooler.fetch
-      records.select { |record| record.complete == 0 }.should be_empty
+    it "Fetches only records that do not have a ResizedOffer record" do
+      offers = Offer.fetch
+      offers.select { |offer| offer.resized_offer != nil }.should be_empty
     end
 	end
+#
+#  describe "#pop" do
+#    before do
+#      @records = OfferImages.init
+#    end
+#
+#    it "Returns the next record needed to be processed" do
+#      current_record = @records.next
+#      current_record.class.should be_eql(Offer)
+#    end
+#
+#    it "Returns the connect data for an offer record" do
+#      current_record = @records.next
+#      current_record.url.should include(".jpg")
+#    end
+#
+#    it "Changes our fetched data by shifting our data in the array" do
+#      expect { @records.next }.to change{ @records.offers.count }.by(-1)
+#    end
+#
+#    #it "Sets the recddddord on the db to complete"
+#  end
+
+end
+
+describe Offer do
+
+  it "Returns only the records that do not have a joined resized_offer entry." do
+    offers = Offer.fetch
+    offers.inspect
+  end
+
 end
